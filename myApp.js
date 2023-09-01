fb10d104d64af9c2fc56c8b38aeeccf044f12c3d
mdkuc <md.kucharek@gmail.com>
2023-09-01 21:32:00 +0200

    function test res.send

--- myApp.js
+++ myApp.js
@@ -1,13 +1,13 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-// app.get ("/",function(req, res) {
-    // res.send('Hello Express');
-//   });
+app.get ("/",function(req, res) {
+    res.send('Hello Express');
+});
 app.get("/", function(req,res) {
     res.sendFile(__dirname + "/views/index.html");
 });
-app.use("public", express.static(__dirname + "/public"));
+// app.use("public", express.static(__dirname + "/public"));
 
 
 
