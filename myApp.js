a7844f79c9d39bfc63d673eb4b766ae2b8317438
mdkuc <md.kucharek@gmail.com>
2023-09-01 21:51:32 +0200

    curl test

--- myApp.js
+++ myApp.js
@@ -1,14 +1,17 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-// app.get ("/",function(req, res) {
-    // res.send('Hello Express');
-// });
-app.get("/", function(req,res) {
-    res.sendFile(__dirname + "/views/index.html");
+app.get ("/",function(req, res) {
+    res.send('Hello Express');
 });
-app.use("/public", express.static(__dirname + "/public"));
+// app.get("/", function(req,res) {
+//     res.sendFile(__dirname + "/views/index.html");
+// });
+// app.use("/public", express.static(__dirname + "/public"));
 
+// app.get("/json", function(res.json) {
+    // "message" : "hello json"
+// });
 
 
 
