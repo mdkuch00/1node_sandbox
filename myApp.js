8e838b8acd6042138db59de97e33b5ef7631ad25
mdkuc <md.kucharek@gmail.com>
2023-09-02 20:41:45 +0200

    JSON send

--- myApp.js
+++ myApp.js
@@ -1,17 +1,19 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-app.get ("/",function(req, res) {
-    res.send('Hello Express');
-});
+// app.get ("/",function(req, res) {
+    // res.send('Hello Express');
+// });
 // app.get("/", function(req,res) {
 //     res.sendFile(__dirname + "/views/index.html");
 // });
 // app.use("/public", express.static(__dirname + "/public"));
 
-// app.get("/json", function(res.json) {
-    // "message" : "hello json"
-// });
+app.get("/json", (req, res) => {
+    res.json({
+        "message" : "hello json"
+    });
+});
 
 
 
