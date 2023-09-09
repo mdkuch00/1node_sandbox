f1ede00177b720d49443d0a6c1add802cb7fc7d7
mdkuc <md.kucharek@gmail.com>
2023-09-09 23:35:18 +0200

    JSON formatting added

--- myApp.js
+++ myApp.js
@@ -2,19 +2,24 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 console.log("Hello World");
-app.get ("/",function(req, res) {
-    res.send('Hello Express');
-});
+// app.get ("/",function(req, res) {
+//     res.send('Hello Express');
+// });
 // app.get("/", function(req,res) {
 //     res.sendFile(__dirname + "/views/index.html");
 // });
 // app.use("/public", express.static(__dirname + "/public"));
 
-// app.get("/json", (req, res) => {
-//     res.json({
-//         "message" : "Hello json"
-//     });
-// });
+app.get("/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE==="uppercase") {
+    res.json({
+        "message" : "Hello json".toUpperCase
+    });}
+    else {
+    res.json({
+        "message" : "Hello json"
+    });}
+});
 
 
 
