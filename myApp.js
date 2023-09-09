a71420646c17962626e1998aa6bfbd66c1f9b726
mdkuc <md.kucharek@gmail.com>
2023-09-09 19:39:24 +0200

    Files removed

--- myApp.js
+++ myApp.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 console.log("Hello World");
-// app.get ("/",function(req, res) {
-    // res.send('Hello Express');
-// });
+app.get ("/",function(req, res) {
+    res.send('Hello Express');
+});
 // app.get("/", function(req,res) {
 //     res.sendFile(__dirname + "/views/index.html");
 // });
 // app.use("/public", express.static(__dirname + "/public"));
 
-app.get.process.env.MESSAGE_STLE("/json", (req, res) => {
-    res.json({
-        "message" : "Hello json"
-    });
-});
+// app.get("/json", (req, res) => {
+//     res.json({
+//         "message" : "Hello json"
+//     });
+// });
 
 
 
